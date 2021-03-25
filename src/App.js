80da5fe30a7f85b8491853255cc8e658daab23f8
samethaz@gmail.com <samethaz@gmail.com>
2021-03-26 00:24:14 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Profil from './component/profil';
+
+function App() {
+  return (
+    <div className="App">
+      <Profil/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/profil.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Profil(){
+    return(
+ <div>
+    <h1>Samet Haz Kisisel Profilim</h1>
+    <h2>Hakkimda</h2>
+    <p>istanbulda dogdum.........</p>
+ </div>
+    )
+
+}
+export default Profil;
